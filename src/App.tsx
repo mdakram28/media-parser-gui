8fd47f3cc058371361b429bd353f39007be025e9
Akram Ansari <mdakram28@gmail.com>
2024-01-14 13:37:33 -0500

    Add GitHub star button to toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,20 @@ import { Switch } from "@mui/material";
 import { RawAnalyzerComponent } from "./formats/raw/raw-analyzer";
 import { HevcAnalyzerComponent } from "./formats/hevc/hevc-analyzer";
 import { Link, NavLink, Route, Routes } from "react-router-dom";
-
+import GitHubButton from 'react-github-btn';
 
 export default function Dashboard() {
   const [page, setPage] = React.useState("av1");
   const menuItems = [{
     page: "av1",
     title: "AV1 parser"
-  },{
+  }, {
     page: "mp4",
     title: "MP4 parser"
-  },{
+  }, {
     page: "raw",
     title: "Raw viewer"
-  },{
+  }, {
     page: "hevc",
     title: "HEVC parser"
   }]
@@ -41,16 +41,24 @@ export default function Dashboard() {
     <main>
       <div className="toolbar">
         <div className="toolbar-item"><b>Multimedia Explorer</b></div>
-        {menuItems.map((menu) => 
-          <NavLink to={"/"+menu.page} key={menu.page} 
-            className={({isActive}) => "toolbar-item " + (isActive && "active")}>
-              {menu.title}
+        {menuItems.map((menu) =>
+          <NavLink to={"/" + menu.page} key={menu.page}
+            className={({ isActive }) => "toolbar-item " + (isActive && "active")}>
+            {menu.title}
           </NavLink>
         )}
-        <span style={{flex: 1}}></span>
+        <span style={{ flex: 1 }}></span>
+        <div className="toolbar-item gh">
+          <GitHubButton
+            href="https://github.com/mdakram28/av1-parser-gui"
+            data-color-scheme="no-preference: dark; light: dark; dark: dark;"
+            data-size="large"
+            aria-label="Star mdakram28/av1-parser-gui on GitHub"
+          >Star</GitHubButton>
+        </div>
         <div className="toolbar-item">
           Light
-          <Switch defaultChecked={true} inputProps={{'aria-label': "Dark"}} onClick={(ev: any) => setTheme(ev.target.checked ? "dark" : "light")}/>
+          <Switch defaultChecked={true} inputProps={{ 'aria-label': "Dark" }} onClick={(ev: any) => setTheme(ev.target.checked ? "dark" : "light")} />
           Dark
         </div>
       </div>
